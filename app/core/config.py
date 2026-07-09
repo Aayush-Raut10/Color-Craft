49207672ca91532b215994d7993bde29742060b4
Aayush-Raut10 <aayushraut209@gmail.com>
2026-07-09 19:37:48 +0545

    Edited configuration

--- app/core/config.py
+++ app/core/config.py
@@ -6,11 +6,5 @@ load_dotenv()
 class Settings:
     PROJECT_NAME: str = "ColorCraft AI"
     HF_API_TOKEN:str = os.getenv("HF_API_TOKEN")
-    
-    MAX_CONCURRENT_IMAGE_GENERATIONS: int = 10
-    GEMINI_MAX_RETRIES: int = 10
-    GEMINI_RETRY_BACKOFF_FACTOR: float = 2.0
-    
-    ENV: str = "production"
 
 settings = Settings()
