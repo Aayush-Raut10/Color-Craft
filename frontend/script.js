6fa7502031063b3a3a02888b1fe481dee7f2d5e7
Aayush-Raut10 <aayushraut209@gmail.com>
2026-07-11 09:36:32 +0545

    fix: Retruned the error that match with backend HttpException details

--- frontend/script.js
+++ frontend/script.js
@@ -31,7 +31,6 @@ form.addEventListener("submit", async (e) => {
 
         if (!response.ok) {
             const error = await response.json();
-            console.log(error);
             throw new Error(error.detail);
         }
         const blob = await response.blob();
