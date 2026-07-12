e29b42820f4200df51341f26504f6106b3fd7c0e
Aayush-Raut10 <aayushraut209@gmail.com>
2026-07-12 10:56:21 +0545

    debugging

--- app/api/v1/endpoints.py
+++ app/api/v1/endpoints.py
@@ -35,7 +35,7 @@ def generate_coloring_book(request: ColoringBookRequest) -> FileResponse:
             )
 
             images.append(image)
-    except Exception:
+    except Exception as e:
         print(e)
         raise HTTPException(status_code=status.HTTP_429_TOO_MANY_REQUESTS, detail="You have reached the free 1-month limit."
 )
